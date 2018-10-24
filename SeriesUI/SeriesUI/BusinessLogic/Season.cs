b1ed17e00e233f35e59ce2823e2ff432b9771de4
Jean Marc Valk <jean.marc.valk@vanderlande.com>
2018-10-24 20:25:39 +0200

    Column headers to set all checkboxes

--- SeriesUI/SeriesUI/BusinessLogic/Season.cs
+++ SeriesUI/SeriesUI/BusinessLogic/Season.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SeriesUI.BusinessLogic
 {
@@ -16,6 +17,30 @@ namespace SeriesUI.BusinessLogic
 
         public List<Episode> Episodes { get; set; }
 
+        public int Completeness
+        {
+            get
+            {
+                var result = (int) CompletenessStates.Complete;
+                var i = -1;
+                while (result < (int) CompletenessStates.NotDownloaded && i < Episodes.Count - 1)
+                {
+                    i++;
+
+                    if (Episodes[i].AirDate.Date >= DateTime.Today.Date) continue;
+
+                    if (!Episodes[i].Downloaded)
+                        result = Math.Max(result, (int) CompletenessStates.NotDownloaded);
+                    else if (!Episodes[i].SubTitles[Episode.SubTitle.NL] && !Episodes[i].SubTitles[Episode.SubTitle.EN])
+                        result = Math.Max(result, (int) CompletenessStates.NotSubbed);
+                    else if (!Episodes[i].SubTitles[Episode.SubTitle.NL])
+                        result = Math.Max(result, (int) CompletenessStates.NotSubbedNl);
+                }
+
+                return result;
+            }
+        }
+
         public void SetAllSubs(Episode.SubTitle sub)
         {
             foreach (var episode in Episodes)
@@ -26,6 +51,38 @@ namespace SeriesUI.BusinessLogic
         {
             foreach (var episode in Episodes)
                 episode.Downloaded = true;
+        }
+
+        public void ToggleAllSubs(Episode.SubTitle subTitle)
+        {
+            // [!] Somehow, using:
+            // c.SubTitles[subTitle] = !Episodes[0].SubTitles[subTitle];
+            // results in all subs having a value unequal to the first
+
+            if (Episodes.Count > 0)
+            {
+                var checkedValue = !Episodes[0].SubTitles[subTitle];
+
+                Episodes.ToList().ForEach(c => c.SubTitles[subTitle] = checkedValue);
+            }
+        }
+
+        public void ToggleDownloaded()
+        {
+            if (Episodes.Count > 0)
+            {
+                var checkedValue = !Episodes[0].Downloaded;
+
+                Episodes.ToList().ForEach(c => c.Downloaded = checkedValue);
+            }
+        }
+
+        private enum CompletenessStates
+        {
+            Complete,
+            NotSubbedNl,
+            NotSubbed,
+            NotDownloaded
         }
     }
 }
