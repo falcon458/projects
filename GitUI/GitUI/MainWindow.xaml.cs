497de5822712cb290f3793c083976b9584c2a7fd
Jean Marc Valk <jean.marc.valk@vanderlande.com>
2018-11-16 15:45:08 +0100

    First GitUI version

---
+++ GitUI/GitUI/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Octokit;
+
+namespace GitUI
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void getGit()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("lsjkdghlsdkjglsdj"));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            getGit();
+        }
+    }
+}
